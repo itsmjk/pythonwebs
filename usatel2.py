222411c3708dae72e133d039a231c67ef58be90a
itsmjk <itsmjkniazi@gmail.com>
2023-11-01 14:44:58 +0500

    Update usatel2.py

--- usatel2.py
+++ usatel2.py
@@ -311,7 +311,7 @@ def scrape_channel_messages(channel_username):
                                         fb_group_response = requests.post(fb_group_api_url, data=fb_group_params)
                                         if fb_group_response.status_code == 200:
                                             print(f"Message posted on the Facebook group. Now wait 360 seconds")
-                                            # time.sleep(360)
+                                            time.sleep(360)
                                         else:
                                             print(f"Error posting message on the Facebook group (ID: {group_id}):", fb_group_response.text)
                                         
@@ -325,7 +325,6 @@ def scrape_channel_messages(channel_username):
                                         fb_response = requests.post(fb_api_url, data=fb_params)
                                         if fb_response.status_code == 200:
                                             print("Message posted on OUR page.")
-                                            time.sleep(360)
                                         else:
                                             print("Error posting message on OUR page:", fb_response.text)
 
