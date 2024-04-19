31189fec77bd574fe8514c783b6f3bff7d2f2a1f
itsmjk <itsmjkniazi@gmail.com>
2024-04-20 00:38:14 +0500

    Update sheet1.py

--- sheet1.py
+++ sheet1.py
@@ -1,7 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import time
-from telethon.sync import TelegramClient, events
+from telethon.sync import TelegramClient
 
 # Define the scope of access and credentials
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -12,88 +12,59 @@ client = gspread.authorize(creds)
 api_id = 24277666
 api_hash = '35a4de7f68fc2e5609b7e468317a1e37'
 session_name = 'sheet1'
+telegram_group_id = -1002010661365
 
 # Initialize a Telethon client
 client_telethon = TelegramClient(session_name, api_id, api_hash)
 
 # Open the Google Spreadsheet by title and worksheet by title
-spreadsheet = client.open("[ARCHIVE] FEBRUARY 24 Inventory")
+spreadsheet = client.open("[CURRENT] April 24 Inventory")
 worksheet = spreadsheet.worksheet("Current inventory list")
 
-# Function to check for new rows and send them to Telegram
-def check_for_new_rows_and_send(previous_rows):
-    current_rows = worksheet.get_all_values()
-    current_row_count = len(current_rows)
-    print(f"Last row number: {current_row_count}")
+# Function to check for changes in specified columns and send them to Telegram
+def check_for_changes_and_send(previous_data):
+    current_data = worksheet.get_all_values()
 
-    new_rows = []
-    edited_rows = []
+    messages = []
+    for row in current_data:
+        d_value = row[3]
+        if any(c.isdigit() for c in d_value) and '+' in d_value or (d_value.isdigit() and int(d_value) > 0):
+            formatted_row = f"{row[4]} {row[0]} ({row[2]})"  # Adjust the index based on the desired columns
+            messages.append(formatted_row)
 
-    # Compare each row of the current and previous rows
-    for i in range(current_row_count):
-        if i >= len(previous_rows):
-            # New row
-            if all(current_rows[i][col] for col in [0, 2, 4]):  # Check if columns A, C, and E have data
-                new_rows.append(current_rows[i])
-        else:
-            if all(current_rows[i][col] for col in [0, 2, 4]):  # Check if columns A, C, and E have data
-                if current_rows[i] != previous_rows[i]:
-                    # Edited row
-                    edited_rows.append((current_rows[i][0], current_rows[i][2], current_rows[i][4]))  # Only select specified columns
+    if messages and messages != previous_data:  # Check if there are changes and the data is different from previous
+        print("Changes detected. Sending message to Telegram...")
+        send_messages_to_telegram(messages)
+        print("Messages sent to Telegram")
 
-    if new_rows:
-        print(f"{len(new_rows)} new row(s) inserted:")
-        send_rows_to_telegram_new(new_rows)
+    return messages
 
-    if edited_rows:
-        print(f"{len(edited_rows)} row(s) edited:")
-        send_rows_to_telegram_edited(edited_rows)
-
-    # Update the previous rows
-    previous_rows = current_rows
-
-    return previous_rows
-
-
-# Function to send rows to Telegram
-def send_rows_to_telegram_new(new_rows):
-    message = ""
-    for row_data in new_rows:
-        formatted_data = f"{row_data[4]} {row_data[0]} ({row_data[2]})"  # Adjust the index based on the desired columns
-        message += f"{formatted_data}\n"
-    print(message)  # Modify this to send the message to Telegram
-    # Send the message to the Telegram channel
+# Function to send messages to Telegram
+def send_messages_to_telegram(messages):
     try:
-        client_telethon.send_message('harnoli7', message)
-        print("Message sent to Telegram")
+        # Construct the message string
+        message = "\n".join(messages)
+        # Split the message into chunks of maximum 4000 characters
+        chunks = [message[i:i+4000] for i in range(0, len(message), 4000)]
+        
+        for chunk in chunks:
+            print(chunk)
+            # client.send_message(telegram_group_id, chunk)
+            time.sleep(1)  # Add a small delay between messages to avoid rate limits
+        
     except Exception as e:
         print(f"Error sending message to Telegram: {e}")
 
-# Function to send rows to Telegram
-def send_rows_to_telegram_edited(edited_rows):
-    message = ""
-    for row_data in edited_rows:
-        formatted_data = f"{row_data[2]} {row_data[0]} ({row_data[1]})"  # Adjust the index based on the desired columns
-        message += f"{formatted_data}\n"
-
-    print(message)  # Modify this to send the message to Telegram
-    # Send the message to the Telegram channel
-    try:
-        client_telethon.send_message('harnoli7', message)
-        print("Message sent to Telegram")
-    except Exception as e:
-        print(f"Error sending message to Telegram: {e}")
-
-# Initial check for new rows
-print("Initial check:")
-previous_rows = worksheet.get_all_values()
+# Initial check for changes
+print("Initial check for changes:")
+previous_data = worksheet.get_all_values()
 
 # Start the Telethon client
 with client_telethon:
-    # Start checking for new rows every 5 seconds
+    # Start checking for changes every 70 seconds
     while True:
         print("Sleep time started.")
         time.sleep(70)
         print("Sleep time ended.")
-        print("Checking for new rows...")
-        previous_rows = check_for_new_rows_and_send(previous_rows)
+        print("Checking for changes...")
+        previous_data = check_for_changes_and_send(previous_data)
