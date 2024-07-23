8ea5a98d4fc5f0150f9ef8597066f01a8b979cb1
itsmjk <itsmjkniazi@gmail.com>
2024-07-23 09:31:11 +0500

    Update link_poster.py

--- link_poster.py
+++ link_poster.py
@@ -14,7 +14,7 @@ def send_message(bot_token, chat_id, message):
 # Replace with your bot token and the new chat id
 bot_token = "7253586168:AAFXyf3i6-QPHNE5nJZ_-Uh-_DmpliN6HXM"
 chat_id = -1001895503131
-message = 'Welcome to Houston, Texas Cannabis Marketplace! Click <a href="https://t.me/+T7e74lLbT6ZmNWUx">here</a> to join Passion Farms Wholesale catalog.'
+message = 'Welcome to Houston, Texas Cannabis Marketplace! Click <a href="https://t.me/+uGN4PpZCUIJiZDUx">here</a> to join Passion Farms Wholesale catalog.'
 
 while True:
     response = send_message(bot_token, chat_id, message)
